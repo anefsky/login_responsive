f39bfa9edbf0f9ebd0ed2cb8f669eadbfe3e06c7
Nefsky <anefsky@01-c02s503gg8wm.webmdhealth.net>
2017-04-27 17:11:44 -0400

    Added submit buttons.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,8 @@
 
+function getElementIndex(item, collection) { // from https://gist.github.com/astur/7c08319a414cb456c581
+    return [].slice.call(collection).indexOf(item);
+}
+
 (function handleFormToggling() {
     var formsWidget = document.querySelector('.forms-widget');
     var tabMenu = formsWidget.querySelector('ul.tabs')
@@ -10,30 +14,33 @@
     singleTabs[defaultSelectedIndex].classList.add('selected');
     singleForms[defaultSelectedIndex].classList.add('selected');
 
-    singleTabs.forEach(function(singleTab) {
-        singleTab.addEventListener('click', function() {
+    for(var i = 0; i < singleTabs.length; i++) {
+        singleTabs[i].addEventListener('click', function() {
             tabMenu.querySelector('li.single-tab.selected').classList.remove('selected'); // clear previous selected
             this.classList.add('selected'); // mark selected tab
             var selectedIndex = getElementIndex(this, singleTabs); // index of selected tab
             formsCollection.querySelector('li.single-form.selected').classList.remove('selected');
             singleForms[selectedIndex].classList.add('selected'); // show selected form
         });
-    });
-})()
-
-function getElementIndex(item, collection) { // from https://gist.github.com/astur/7c08319a414cb456c581
-    return [].slice.call(collection).indexOf(item);
-}
+    };
+})();
 
 (function handleShowHidePassword() {
-    var form = document.querySelectorAll('li.single-form')[0];
+    var form = document.querySelectorAll('li.single-form')[0]; // just needed for first form
     var container = form.querySelector('span.password-input-container');
     var toggler = container.querySelector('a');
     var passwordInput = container.querySelector('input');
 
     toggler.addEventListener('click', function() {
-        console.log('clicked');
-        passwordInput.setAttribute('type', 'text');
-        passwordInput.style.borderColor = "red";
+        var inputType, caption;
+        if(passwordInput.getAttribute('type') === 'password') {
+            inputType = 'text';
+            caption = 'hide';
+        } else {
+            inputType = 'password';
+            caption = 'show';
+        }
+        passwordInput.setAttribute('type', inputType);
+        toggler.innerHTML = caption;
     });
 })();
